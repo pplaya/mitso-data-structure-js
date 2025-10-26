cfcd2dc999d444d1a548663af236409be853d735
pplaya <gleb19051904@gmail.com>
2025-10-27 01:36:23 +0300

    feat: stack completed

--- src/stack.js
+++ src/stack.js
@@ -1,30 +1,29 @@
 import { NotImplementedError } from "../extensions/index.js";
 
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
+/*
+Implement the Stack with a given interface via array.
  */
+
 export default class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
   }
 
   pop() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    if (this.items.length === 0) {
+      return undefined;
+    }
+    return this.items.pop();
   }
 
   peek() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    if (this.items.length === 0) {
+      return undefined;
+    }
+    return this.items[this.items.length - 1];
   }
 }
