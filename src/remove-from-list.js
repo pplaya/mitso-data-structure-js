c0797c181d1af381d9370e95feecfe8b04f3d804
pplaya <gleb19051904@gmail.com>
2025-10-27 00:38:17 +0300

    feat: removeFromList completed

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -3,23 +3,24 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
- *
- * @param {List} l
- * @param {Number} k
- * @return {List}
- *
- * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
- * the output should be [1, 2, 4, 5]
- *
- * Singly - linked lists are already defined with this interface
- * function ListNode(x) {
- *   this.value = x;
- *   this.next = null;
- * }
  */
 
-export default function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default function removeKFromList(l, k) {
+  if (!l) return null;
+
+  while (l && l.value === k) {
+    l = l.next;
+  }
+
+  if (!l) return null;
+
+  for (let current = l; current.next !== null; ) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return l;
 }
